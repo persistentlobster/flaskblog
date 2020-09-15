685b81516f61cc1cb110ef32b1107f89c44b10b2
Micah Burnett <persistentlobster@gmail.com>
2020-09-15 15:15:44 -0700

    Redirect user after login to page from query param

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
@@ -54,7 +54,9 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        
         else:
             flash('Login unsuccessful. Please Check email and password', 'danger')
 
