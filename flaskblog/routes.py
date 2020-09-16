71cdef15b3937a3fbb4106642db1a91c4ccd4660
Micah Burnett <persistentlobster@gmail.com>
2020-09-15 17:42:38 -0700

    Save updated profile pics to db and filesystem

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,8 @@ from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
 
 posts = [
     {
@@ -67,12 +69,29 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-# @login_required --> login route set in init file
+
+def save_picture(form_picture):
+    # Generate random unique filename to avoid collisions in db.
+    # Save original file extension and discard original file name.
+    # Save form_picture (a filestorage instance) profile_pics/ subdir.
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    # @login_required --> login route set in init file
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
