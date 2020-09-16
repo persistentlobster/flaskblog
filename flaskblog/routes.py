5774963a710b0a26ff7049cf03024d79a2370326
Micah Burnett <persistentlobster@gmail.com>
2020-09-15 20:54:59 -0700

    Resize profile pic on upload

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,10 +1,12 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
-import secrets
-import os
+
 
 posts = [
     {
@@ -21,14 +23,17 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', posts=posts, title='About')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -47,6 +52,7 @@ def register():
 
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -64,6 +70,7 @@ def login():
 
     return render_template('login.html', title='Login', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -78,7 +85,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
@@ -87,6 +98,7 @@ def save_picture(form_picture):
 @login_required
 def account():
     # @login_required --> login route set in init file
+    # TODO: Delete old profile pic when uploading new one
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
