8697d1d1449e07711e15821647ec7acc0b644fe4
Micah Burnett <persistentlobster@gmail.com>
2020-09-17 15:08:36 -0700

    Create app from function passing in config object
    This allows us to deploy different instances of the app (i.e. dubug, prod, etc)

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,7 +1,7 @@
 import os
 import secrets
-from flask import url_for
-from flaskblog import app, mail
+from flask import url_for, current_app
+from flaskblog import mail
 from PIL import Image
 from flask_mail import Message
 
@@ -13,7 +13,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblog import app # imports from init file within package
+from flaskblog import create_app # imports from init file within package
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
