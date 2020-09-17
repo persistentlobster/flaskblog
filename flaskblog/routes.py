2551b08cd6cd3f9c66b8cd65ab2f7eeab3486a6d
Micah Burnett <persistentlobster@gmail.com>
2020-09-16 17:28:11 -0700

    Sort posts by date in descending order

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -28,7 +28,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5)
     return render_template('home.html', posts=posts, page=page)
 
 
